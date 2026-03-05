ceb19cb0d033911da18e0e9fd5a8dc3764174472
AVG <glazyrinav1986@yandex.ru>
2026-03-05 10:31:30 +0300

    feat(aside): добавлен заголовок

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,6 +4,7 @@ import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 import { useRef, useState } from 'react';
+import { Text } from 'src/ui/text';
 
 export const ArticleParamsForm = () => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -20,6 +21,9 @@ export const ArticleParamsForm = () => {
 				ref={containerRef}
 				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
+					<Text weight={800} uppercase size={31}>
+						<>Задайте параметры</>
+					</Text>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
