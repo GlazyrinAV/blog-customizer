778b96b9c582c0d7a7dc8532558771fc17af935a
AVG <glazyrinav1986@yandex.ru>
2026-03-05 09:20:00 +0300

    feat(aside): реализовано открытие и закрытие aside

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,24 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useRef, useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const containerRef = useRef<HTMLDivElement | null>(null);
+
+	const handleClick = () => {
+		setIsOpen(!isOpen);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={handleClick} />
+			<aside
+				ref={containerRef}
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
